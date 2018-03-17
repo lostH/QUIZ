d231a499830ea2d71c81150b22e984623d02228c
Henry Buñay <34858745+lostH@users.noreply.github.com>
2018-03-17 23:50:09 +0100

    update js and update html

--- trabajo/js/script.js
+++ trabajo/js/script.js
@@ -1,11 +1,39 @@
 
-function pasarVariables(pagina, nombres) {
-  pagina +="?";
-
+function pasarVariables(pagina, conteo) {
  
-  pagina += nombres + "=" + escape(eval(nomVec[i]));
-  pagina = pagina.substring(0,pagina.length-1);
-  
+ alert(conteo);
+// obtener valor de la pegunta 1
+  var valor = document.getElementById('correcto1').checked;  
+  if (valor) {
+  		conteo++;
+  }
+// obtener valor de la pegunta 2
+
+	valor = document.getElementById('correcto2').checked;
+  if (valor) {
+  		conteo++;
+  }
+ // obtener valor de la pegunta 3
+  valor = document.getElementById('correcto3').checked;
+  if (valor) {
+  		conteo++;
+  }
+  // obtener valor de la pegunta 4
+  valor = document.getElementById('correcto4').checked;
+  if (valor) {
+  		conteo++;
+  }
+  // obtener valor de la pegunta 5
+  valor = document.getElementById('correcto5').checked;
+  if (valor) {
+  		conteo++;
+  }
+
+alert(conteo);
+  pagina +='?var1='+conteo;
+
+
+
   location.href=pagina;
 }
 
